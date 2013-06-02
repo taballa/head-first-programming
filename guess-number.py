d255161854e083b2b08c9483363cd86f4d8bf135
taballa-hp-mini <taballa@gmail.com>
2013-06-02 23:12:55 +0800

    update while of guess-number.py

--- guess-number.py
+++ guess-number.py
@@ -1,11 +1,11 @@
 print('hello')
-_x = input('Guess the number: ')
-guess = int(_x)
+guess = 0
 while guess != 5:
+    _x = input('Guess the number: ')
+    guess = int(_x)
     if guess > 5:
         print('Too high')
     else:
         print('Too low')
-    guess = int(input('Guess the number: '))
 print('You win')
 print('good bye')
