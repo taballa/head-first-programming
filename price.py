0be292f8f7bbd5db9270acf15e4afa8d3e373d65
taballa-hp-mini <taballa@gmail.com>
2013-06-06 17:03:16 +0800

    add find price (find keyword position)

--- price.py
+++ price.py
@@ -1,13 +1,23 @@
+# coding=utf8
 import urllib2, time, codecs
 
 i = 0
-URL = "http://www.douban.com"
+key = u'¥</span>'
+# URL = "http://www.douban.com"
+URL = "http://www.gome.com.cn"
 while i < 3:
     time.sleep(1)
     page = urllib2.urlopen(URL)
     text = page.read().decode('utf8') # to Unicode
-    text_gbk = text.encode('gbk', 'ignore')
-    print text_gbk
+    where = text.find(key)    
+    start_of_price = where + 8
+    end_of_price = start_of_price + 300
+    price = text[start_of_price:end_of_price]
+    price_gbk = price.encode('gbk', 'ignore')
+    print start_of_price
+    print end_of_price
+    print price
+    print price_gbk
     i = i + 1
 
 print("bye!")
