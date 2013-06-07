8952aef5f6566b7add1ca853c4ba8bdea0e07cae
taballa-hp-mini <taballa@gmail.com>
2013-06-07 20:20:22 +0800

    add repeat function and use raw_input

--- price.py
+++ price.py
@@ -1,23 +1,32 @@
 # coding=utf8
-import urllib2, time, codecs
 
-i = 0
-key = u'¥</span>'
-# URL = "http://www.douban.com"
-URL = "http://www.gome.com.cn"
-while i < 3:
-    time.sleep(1)
-    page = urllib2.urlopen(URL)
-    text = page.read().decode('utf8') # to Unicode
-    where = text.find(key)    
-    start_of_price = where + 8
-    end_of_price = start_of_price + 300
-    price = text[start_of_price:end_of_price]
-    price_gbk = price.encode('gbk', 'ignore')
-    print start_of_price
-    print end_of_price
-    print price
-    print price_gbk
-    i = i + 1
+def price():
+    import urllib2, time, codecs
+    i = 0
+    key = u'¥</span>'
+    # URL = "http://www.douban.com"
+    URL = "http://www.gome.com.cn"
+    while i < 2:
+        time.sleep(1)
+        page = urllib2.urlopen(URL)
+        text = page.read().decode('utf8') # to Unicode
+        where = text.find(key)    
+        start_of_price = where + 8
+        end_of_price = start_of_price + 300
+        price = text[start_of_price:end_of_price]
+        price_gbk = price.encode('gbk', 'ignore')
+        print start_of_price
+        print end_of_price
+        print price
+        print price_gbk
+        i = i + 1
+    print("bye!")
 
-print("bye!")
+print("Hello Welcome to ???")
+
+repeat = 'y'
+
+while repeat == 'y':
+    price()
+    _x = raw_input('Do you repeat? (Y/N)')
+    repeat = str(_x)
