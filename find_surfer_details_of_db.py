2425996d28fa491e1c350f0000de7d1e6112adc0
taballa.hp-PD <taballa@gmail.com>
2013-06-10 17:24:03 +0800

    update

--- find_surfer_details_of_db.py
+++ find_surfer_details_of_db.py
@@ -7,8 +7,27 @@ def find_details(id2find):
     cursor.execute("select * from surfers")
     rows = cursor.fetchall()
     for row in rows:
-        if row['id'] == 104:
-            print("ID is " + str(row['id']))
-            print("Name is " + row['name'])
-            print("Board-type is " + row['board'])
+        if row['id'] == id2find:
+            s = {}
+            s['id'] = str(row['id'])
+            s['name'] = row['name']
+            s['country'] = row['country']
+            s['average'] = str(row['average'])
+            s['board'] = row['board']
+            s['age'] = str(row['age'])
             cursor.close()
+            return(s)
+    cursor.close()
+    return({})
+
+lookup_id = int(input("Enter the id of the surfer: "))
+surfer = find_details(lookup_id)
+if surfer:
+    print("ID:       " + surfer['id'])
+    print("Name:     " + surfer['name'])
+    print("Country:  " + surfer['country'])
+    print("Average:  " + surfer['average'])
+    print("Board:    " + surfer['board'])
+    print("Age:      " + surfer['age'])
+else:
+    print("Have not found!")
