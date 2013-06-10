d178877905e9ffafe5dfc9a3156904299d41d672
taballa.hp-PD <taballa@gmail.com>
2013-06-10 13:57:44 +0800

    hash_scores sorted()

--- high_score.py
+++ high_score.py
@@ -2,11 +2,13 @@ result_f = open("results.txt")
 
 high_score = 0
 scores = []
+hash_scores = {}
 
 for line in result_f:
     (name, score) = line.split()
     score = float(score)
     scores.append(score)
+    hash_scores[score] = name
     if score > high_score:
         high_score = score
 
@@ -14,6 +16,9 @@ result_f.close()
 # scores.sort()
 # scores.reverse()
 scores.sort(reverse = True)
+
+for each_score in sorted(hash_scores.keys(), reverse = True):
+    print('Surfer ' + hash_scores[each_score] + ' scored ' + str(each_score))
 
 print("The highest score was: " + str(high_score))
 print(scores[0])
