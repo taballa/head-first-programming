29c6256f5b5424c03d6f42039e785809c067b5a5
taballa.hp-PD <taballa@gmail.com>
2013-06-10 17:26:03 +0800

    update text.py fetch www.jd.com ￥ for gbk

--- text.py
+++ text.py
@@ -1,26 +1,28 @@
-    # coding=utf8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+import urllib2, time
 def get_price():
-    import urllib2, time, codecs
     i = 0
-    key = u'¥</span>'
-    # URL = "http://www.douban.com"
-    URL = "http://www.gome.com.cn"
+    # URL = "http://www.douban.com" # utf8
+    # URL = "http://www.gome.com.cn" # utf8
+    URL = "http://www.jd.com" # gbk
+    key = u'￥' # end_of_price = 1
     while i < 2:
         print("Today discounted price is: ")
-        time.sleep(1)
         page = urllib2.urlopen(URL)
-        text = page.read().decode('utf8') # to Unicode
+        text = page.read().decode('gbk') # to Unicode
         where = text.find(key)    
-        start_of_price = where + 8
-        end_of_price = start_of_price + 300
+        start_of_price = where + 1
+        end_of_price = start_of_price + 6
         price = text[start_of_price:end_of_price]
         # price_gbk = price.encode('gbk', 'ignore')
         print start_of_price
         print end_of_price
-        print price
         # print price_gbk
+        print price # 跳出循环
         i = i + 1
+        time.sleep(1)
     print("bye!")
 
 print("Hello Welcome to Supermarket.")
@@ -31,3 +33,9 @@ while repeat == 'y':
     get_price()
     _x = raw_input('Do you repeat? (Y/N)')
     repeat = str(_x)
+
+def send_to_mail():
+   msg = "I am a message that will be sent to your email." 
+   password_manager = urllib2.HTTPPasswordMgr()
+   password_manager.add_password("douban API", "http://www.douban.com/", "tel800810@gmail.com", "qq440106")
+   http_handle = urllib2.HTTPBasicAuthHandler(password_manager)
