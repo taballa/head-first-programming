10692307ee8b57e19d499d900cc3df36dcc4dd0c
taballa.hp-PD <taballa@gmail.com>
2013-06-10 13:24:07 +0800

    sort scores

--- high_score.py
+++ high_score.py
@@ -1,11 +1,21 @@
 result_f = open("results.txt")
 
 high_score = 0
+scores = []
 
 for line in result_f:
-    if float(line) > high_score:
-        high_score = float(line)
+    (name, score) = line.split()
+    score = float(score)
+    scores.append(score)
+    if score > high_score:
+        high_score = score
 
 result_f.close()
+# scores.sort()
+# scores.reverse()
+scores.sort(reverse = True)
 
 print("The highest score was: " + str(high_score))
+print(scores[0])
+print(scores[1])
+print(scores[2])
