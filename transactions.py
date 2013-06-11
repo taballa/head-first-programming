476f7f56dde9575fa2f3c93bedc676fdec2e982d
taballa.hp-PD <taballa@gmail.com>
2013-06-11 20:12:54 +0800

    change credit_card and price order

--- transactions.py
+++ transactions.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 def save_transaction(price, credit_card, description):
     file = open("transactions.txt", "a") # "a"表示把文件添加到结尾
-    file.write("%16s%07d%s\n" % (credit_card, price*100, description))
+    file.write("%07d%16s%s\n" % (price*100, credit_card, description))
     file.close()
 
