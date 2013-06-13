4f4175210fcc19c1c3a1ff756814511bca071787
taballa.hp-PD <taballa@gmail.com>
2013-06-13 23:16:00 +0800

    read *.wav files name added SoundPanel(). OVER!!

--- sound_player.py
+++ sound_player.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from sound_panel import *
 from Tkinter import *
 import pygame.mixer
+import os
+
 
 mixer = pygame.mixer
 mixer.init()
@@ -8,10 +13,16 @@ mixer.init()
 app = Tk()
 app.title("Head First Mix")
 
-panel = SoundPanel(app, mixer, "50459_M_RED_Nephlimizer.wav")
-panel.pack()
-panel = SoundPanel(app, mixer, "49119_M_RED_HardBouncer.wav")
-panel.pack()
+dirList = os.listdir('.') # 获取当前目录下所有文件
+for fname in dirList:
+    if fname.endswith('.wav'):
+        panel = SoundPanel(app, mixer, fname)
+        panel.pack()
+
+# panel = SoundPanel(app, mixer, "50459_M_RED_Nephlimizer.wav")
+# panel.pack()
+# panel = SoundPanel(app, mixer, "49119_M_RED_HardBouncer.wav")
+# panel.pack()
 
 create_gui(app, mixer, "50459_M_RED_Nephlimizer.wav")
 create_gui(app, mixer, "49119_M_RED_HardBouncer.wav")
