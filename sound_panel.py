d06991c38efcb014cbbd30b41c84f8f75b759498
taballa.hp-PD <taballa@gmail.com>
2013-06-13 22:50:37 +0800

    complate class SoundPanel

--- sound_panel.py
+++ sound_panel.py
@@ -1,5 +1,40 @@
 from Tkinter import *
 
+class SoundPanel(Frame):
+    """docstring for SoundPanel"""
+    def __init__(self, app, mixer, sound_file):
+        # super(SoundPanel, self).__init__() # automatic adds
+        Frame.__init__(self, app) # NOTE: why initialization Frame and use app parameter?
+        self.track = mixer.Sound(sound_file) # NOTE: why everyone variable used self?
+        self.track_playing = IntVar()
+        # self.app = app # automatic adds
+        track_button = Checkbutton(self, variable = self.track_playing, command = self.track_toggle, text = sound_file) # NOTE: why used self not use app
+        track_button.pack(side = LEFT)
+
+        # NOTE: 看来是，变量用 self 开头，volume_scale 和 track_button 之类的组件不用 self 开头
+        self.volume = DoubleVar()
+        self.volume.set(self.track.get_volume())
+        volume_scale = Scale(
+            self,
+            variable = self.volume,
+            from_ = 0.0,
+            to = 1.0,
+            resolution = 0.1,
+            command = self.change_volume,
+            label = "Volume",
+            orient = HORIZONTAL)
+        volume_scale.pack(side = RIGHT)
+
+    def track_toggle(self):
+        if self.track_playing.get() == 1:
+            self.track.play(loops = -1)
+        else:
+            self.track.stop()
+
+    def change_volume(self, v):
+        self.track.set_volume(self.volume.get())
+
+
 def create_gui(app, mixer, sound_file):
     track = mixer.Sound(sound_file)
 
@@ -13,8 +48,8 @@ def create_gui(app, mixer, sound_file):
         track.set_volume(volume.get())
 
     track_playing = IntVar()
-    Checkbutton(app, variable = track_playing, command = track_toggle, text = sound_file).pack()
-    
+    Checkbutton(app, variable = track_playing, command = track_toggle, text = sound_file).pack(side = LEFT)
+
     volume = DoubleVar()
     volume.set(track.get_volume())
     volume_scale = Scale(
@@ -26,6 +61,6 @@ def create_gui(app, mixer, sound_file):
         command = change_volume,
         label = "Volume",
         orient = HORIZONTAL)
-    volume_scale.pack()
+    volume_scale.pack(side = RIGHT)
 
 
--- sound_player.py
+++ sound_player.py
@@ -1,10 +1,19 @@
-from sound_panel import create_gui
+from sound_panel import *
 from Tkinter import *
 import pygame.mixer
 
 mixer = pygame.mixer
+mixer.init()
 
 app = Tk()
 app.title("Head First Mix")
 
+panel = SoundPanel(app, mixer, "50459_M_RED_Nephlimizer.wav")
+panel.pack()
+panel = SoundPanel(app, mixer, "49119_M_RED_HardBouncer.wav")
+panel.pack()
+
 create_gui(app, mixer, "50459_M_RED_Nephlimizer.wav")
+create_gui(app, mixer, "49119_M_RED_HardBouncer.wav")
+
+app.mainloop()
